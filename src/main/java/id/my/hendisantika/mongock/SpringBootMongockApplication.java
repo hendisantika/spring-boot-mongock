f109901a814d1ff5f014f5eb4280c804e988e644
hendisantika <hendisantika@yahoo.co.id>
2024-11-26 16:23:32 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongock/SpringBootMongockApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.mongock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongockApplication.class, args);
+    }
+
+}

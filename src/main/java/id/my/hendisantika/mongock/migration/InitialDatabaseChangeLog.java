f2850d7a447a58245cb10ec10320d7f1e815929d
hendisantika <hendisantika@yahoo.co.id>
2024-11-26 16:29:09 +0700

    Add InitialDatabaseChangeLog🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongock/migration/InitialDatabaseChangeLog.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.mongock.migration;
+
+import io.mongock.api.annotations.ChangeUnit;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mongock
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/11/24
+ * Time: 16.28
+ * To change this template use File | Settings | File Templates.
+ */
+@ChangeUnit(order = "001", id = "user-seeder", author = "hendisantika")
+public class InitialDatabaseChangeLog {
+}
